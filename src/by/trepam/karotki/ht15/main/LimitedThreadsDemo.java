ed5b45642980017574a5de3843b0f83e14a0d6a3
Viachaslau Karotki <Karotkiv@gmail.com>
2016-07-06 15:23:00 +0300

    commit

--- src/by/trepam/karotki/ht15/main/LimitedThreadsDemo.java
+++ src/by/trepam/karotki/ht15/main/LimitedThreadsDemo.java
@@ -3,7 +3,7 @@ package by.trepam.karotki.ht15.main;
 import by.trepam.karotki.ht15.exception.MatrixException;
 import by.trepam.karotki.ht15.part_2.LimitedThreadsMatrixMult;
 
-public class FixedThreadsDemo {
+public class LimitedThreadsDemo {
 	public final static int[][] FIRST_MATRIX = { { 1, 2, 3 }, { 1, 3, 2 }, { 3, 2, 6 } };
 	public final static int[][] SECOND_MATRIX = { { 1, 2, 3 }, { 1, 3, 2 }, { 3, 2, 6 } };
 	public final static int AMOUNT_THREADS = 2;
